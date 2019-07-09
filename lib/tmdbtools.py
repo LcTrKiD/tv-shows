e44f5d71f1d68df0d7115cd008aef69de789acae
lctrkid <lctrkid@mac.com>
2019-07-10 00:45:01 +0200

    Added _get_imdb_id function

--- lib/tmdbtools.py
+++ lib/tmdbtools.py
@@ -8,25 +8,37 @@ from lib.apitool import APITool, APIToolKeyError
 class TMDBError(Exception):
     pass
 
+
 class TMDBTools(object):
-    def __init__(self, api_key: str, tv_show: str):
+    def __init__(self, api_key: Optional[str] = None):
         self.headers = {'Content-Type': 'application/json',
                         'Accept': 'application/json',
                         'Connection': 'close'}
         self.base_uri = 'https://api.themoviedb.org'
-        self.path = '/search/tv'
         self.version = '3'
         self.api_key = api_key
-        self.tv_show = tv_show
-        self.params = {'query': self.tv_show}
+        self.apitool = APITool.create(self)
 
-    def _get_tmdb_id(self) -> Optional[str]:
-        apitool = APITool(self.headers, self.base_uri, self.api_key)
-        response = apitool._GET(self.path, self.version, self.params)
-        if response['results'] == None:
+    def _get_tmdb_id(self, tv_show: str) -> Optional[int]:
+        path = '/search/tv'
+        params = {'query': tv_show}
+        response = self.apitool._GET(path, self.version, params)
+        if response['results'] is None:
             return None
-        id = [r['id'] for r in response['results'] 
-              if r['name'] == self.tv_show]
+        id = [r['id'] for r in response['results']
+              if r['name'] == tv_show]
         if id == []:
             raise TMDBError('TV Show not found')
         return id[0]
+
+    def _get_imdb_id(self, id: int) -> Optional[int]:
+        path = f'/tv/{id}/external_ids'
+        try:
+            response = self.apitool._GET(path,
+                                         self.version)
+        except:
+            raise TMDBError('TV ID incorrect')
+        if response is None:
+            return None
+        id = response['imdb_id'].replace('t', '')
+        return id
