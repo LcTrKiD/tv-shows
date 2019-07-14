ccace7ecebedb6addd4bbe00e38484fa2d20394a
lctrkid <lctrkid@mac.com>
2019-07-14 21:52:17 +0200

    Refactored tmdbtools for the new apitools

--- lib/tmdbtools.py
+++ lib/tmdbtools.py
@@ -17,12 +17,15 @@ class TMDBTools(object):
         self.base_uri = 'https://api.themoviedb.org'
         self.version = '3'
         self.api_key = api_key
-        self.apitool = APITool.create(self)
+        if not self.api_key:
+            raise APIToolKeyError('API key is missing')
+        else:
+            self.apitool = APITool.create(self)
 
     def _get_tmdb_id(self, tv_show: str) -> Optional[int]:
-        path = '/search/tv'
+        path = f'/{self.version}/search/tv'
         params = {'query': tv_show}
-        response = self.apitool._GET(path, self.version, params)
+        response = self.apitool._GET(path, params)
         if response['results'] is None:
             return None
         id = [r['id'] for r in response['results']
@@ -32,10 +35,9 @@ class TMDBTools(object):
         return id[0]
 
     def _get_imdb_id(self, id: int) -> Optional[int]:
-        path = f'/tv/{id}/external_ids'
+        path = f'/{self.version}/tv/{id}/external_ids'
         try:
-            response = self.apitool._GET(path,
-                                         self.version)
+            response = self.apitool._GET(path)
         except:
             raise TMDBError('TV ID incorrect')
         if response is None:
