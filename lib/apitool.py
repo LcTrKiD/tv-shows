4b895b6f8bfb72aaa38f15c210abb33ec48dc646
lctrkid <lctrkid@mac.com>
2019-07-14 21:50:49 +0200

    Refactored apitool to be more generic

--- lib/apitool.py
+++ lib/apitool.py
@@ -17,28 +17,27 @@ class APITool(object):
 
     @staticmethod
     def create(self) -> object:
-        return APITool(self.headers, self.base_uri, self.api_key)
+        if self.api_key:
+            return APITool(self.headers, self.base_uri, self.api_key)
+        return APITool(self.headers, self.base_uri)
 
-    def _get_complete_url(self, version: str, path: str) -> str:
-        return f'{self.base_uri}/{version}{path}'
+    def _get_complete_url(self, path: str) -> str:
+        return f'{self.base_uri}{path}'
 
-    def _get_params(self, params: Optional[dict]) -> dict:
-        if not self.api_key:
-            raise APIToolKeyError
-
-        api_dict = {'api_key': self.api_key}
-        if params:
-            params.update(api_dict)
-        else:
-            params = api_dict
+    def _get_params(self, params: Optional[dict]) -> Optional[dict]:
+        if self.api_key:
+            api_dict = {'api_key': self.api_key}
+            if params:
+                params.update(api_dict)
+            else:
+                params = api_dict
         return params
 
     def _request(self, method: str, path: str,
-                 version: str,
                  params: Optional[dict] = None,
                  payload: Optional[dict] = None,
                  headers: Optional[dict] = None) -> dict:
-        url = self._get_complete_url(version, path)
+        url = self._get_complete_url(path)
         params = self._get_params(params)
 
         response = requests.request(
@@ -50,20 +49,21 @@ class APITool(object):
         response.encoding = 'utf-8'
         return response.json()
 
-    def _GET(self, path: str, version: str,
+    def _GET(self, path: str,
              params: Optional[dict] = None) -> dict:
-        return self._request('GET', path, version, params=params)
+        return self._request('GET', path,
+                             params=params)
 
-    def _POST(self, path: str, version: str,
+    def _POST(self, path: str,
               params: Optional[dict] = None,
               payload: dict = None) -> dict:
-        return self._request('POST', path, version,
+        return self._request('POST', path,
                              params=params,
                              payload=payload)
 
-    def _DELETE(self, path: str, version: str,
+    def _DELETE(self, path: str,
                 params: Optional[dict] = None,
-                payload=None) -> dict:
-        return self._request('DELETE', path, version,
+                payload: dict = None) -> dict:
+        return self._request('DELETE', path,
                              params=params,
                              payload=payload)
